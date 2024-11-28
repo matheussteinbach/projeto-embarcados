e8b9078f250ee0ee6ad3cf6430f9eeab694c11cf
Davi Vitorino da Silva <davi.silva@softplan.com.br>
2024-11-28 19:39:20 -0300

    feat: adiciona endpoint de controle

--- backend/controle/index.js
+++ backend/controle/index.js
@@ -13,6 +13,9 @@ const db = new sqlite3.Database('./dados.db', (err) => {
 db.run(
   `
     CREATE TABLE IF NOT EXISTS controle (
+      codigo INT UNIQUE NOT NULL,
+      valor_limite NUMERIC(10) NOT NULL,
+      PRIMARY KEY (codigo)
     )
   `,
   [],
@@ -24,11 +27,47 @@ db.run(
   }
 );
 
+db.get('SELECT (count(1) > 0) AS existe FROM controle', [], (err, result) => {
+  if (!result.existe) {
+    db.run('INSERT INTO controle VALUES (1, 0)');
+  }
+});
+
 const app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 
-// API
+app.get('/controle', (req, res, next) => {
+  db.get('SELECT valor_limite FROM controle WHERE codigo = ?', [1], (err, result) => {
+    if (err) {
+      res.status(500).send('Ocorreu um erro ao obter a configuração.');
+      return;
+    }
+    if (!result) {
+      res.status(404).send('A configuração de controle não foi encontrada.');
+      return;
+    }
+    res.status(200).send(result.valor_limite.toString());
+  })
+});
+
+app.patch('/controle', (req, res, next) => {
+  db.run(
+    'UPDATE controle SET valor_limite = ? WHERE codigo = ?',
+    [req.body.valor_limite, 1],
+    function (err) {
+      if (err) {
+        res.status(500).send('Ocorreu um erro ao atualizar a configuração.');
+        return;
+      }
+      if (this.changes == 0) {
+        res.status(404).send('A configuração de controle não foi encontrada.');
+        return;
+      }
+      res.status(200).send('Configuração atualizada com sucesso!');
+    }
+  )
+});
 
 const porta = 9010;
 app.listen(porta, () => console.log('API de controle UP'));
